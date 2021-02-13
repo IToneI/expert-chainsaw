73db180d91707b1a2eb840b8e48c0782e593025a
Aliaksei Parkalau <a.parkalov@gmail.com>
2021-02-13 23:18:43 +0300

    add comments with the task

--- HW/HW.cpp
+++ HW/HW.cpp
@@ -3,4 +3,14 @@
 int main()
 {
     std::cout << "Hello World!\n";
+
+    //TODO: create structure matrix {m, n, pointer to array of doubles}
+
+    //TODO: create a 10x10 matrix, fill with 1's
+
+    //TODO: create 10x1 matrix named 'vector', initialize with pi / 2 values
+
+    //TODO: multiply matrix to vector and calculate its length
+
+    //TODO: print answer
 }
